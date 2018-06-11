halogenOS/android_system_vold

vold: Don't compile tests unless explicitly asked for
Change-Id: I2ed687b9824616ae90f580313b0c0d5df9754f14
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

tests/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(BUILD_VOLD_TEST),true)
+
 # Build the unit tests.
 LOCAL_PATH := $(call my-dir)
 
@@ -37,3 +39,5 @@ LOCAL_MODULE := vold_cryptfs_scrypt_hidlization_equivalence_test
 LOCAL_MODULE_TAGS := eng tests
 
 include $(BUILD_NATIVE_TEST)
+
+endif

PotatoProject/system_vold

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

tests/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(BUILD_VOLD_TESTS),true)
+
 # Build the unit tests.
 LOCAL_PATH := $(call my-dir)
 
@@ -37,3 +39,5 @@ LOCAL_MODULE := vold_cryptfs_scrypt_hidlization_equivalence_test
 LOCAL_MODULE_TAGS := eng tests
 
 include $(BUILD_NATIVE_TEST)
+
+endif